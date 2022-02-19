017848be7b77039067223a1178143080f4e05978
Sooraj Yadav <frankSoorj@gmail.com>
2022-02-19 16:28:43 +0530

    the - this

--- weather-app/src/components/app/notFound.js
+++ weather-app/src/components/app/notFound.js
@@ -5,7 +5,7 @@ const NotFound = () => {
   return (
     <div class="row">
         <div class="col-md-12 col-sm-12">
-          <h3 className='d-flex justify-content-center mt-5'>Sorry, the specified city was not found..</h3>
+          <h3 className='d-flex justify-content-center mt-5'>Sorry, this specified city was not found..</h3>
         </div>
     </div>
   );
